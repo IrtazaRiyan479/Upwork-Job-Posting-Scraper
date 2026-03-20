fd955bf78a0c1f64fe89ed107a78f1108bb74943
Irtaza Riyan <martinirtaza@gmail.com>
2026-03-20 07:47:10 +0500

    Refactor condition checks for raw_data and links

--- core/scraper.py
+++ core/scraper.py
@@ -22,14 +22,12 @@ TARGET_URLS = [
 def main():
     for url in TARGET_URLS:
         raw_data = BrightData.get_data(url)
-        if raw_data:
+        if raw_data is not None:
             new_open_jobs_links = ContentParser.check_new_open_jobs(raw_data)
-
-        if new_open_jobs_links:
+            if new_open_jobs_links:
             for link in new_open_jobs_links:
                 new_open_job_data = ContentParser.read_job(BrightData.get_data(link))
                 SendMessage.send_notification(new_open_job_data, link)
-
 
 if __name__ == "__main__":
     main()
